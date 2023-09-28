a74bea759d4ebe084aa628905d12434a0f2f141a
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-09-27 20:30:45 -0500

    Agregue useLocation()

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
+//Componente principal de la aplicación
 import './App.css'
-import { Route, Routes, useLocation} from 'react-router-dom'
+import { Route, Routes, useLocation} from 'react-router-dom' //componentes y hooks necesarios para el enrutamiento 
 import Landing from './components/Landing/Landing'
 import Home from './components/Home/Home'
 import Form from './components/Form/Form'
@@ -8,7 +9,7 @@ import Nav from './components/Nav/Nav'
 
 
 function App() {
-  const location = useLocation()  
+  const location = useLocation()  //obtiene la ubicación actual
 
   return (
     <>
