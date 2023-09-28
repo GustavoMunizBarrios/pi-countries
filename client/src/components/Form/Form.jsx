97ba97d9f989e84c20cd9e38ae147b31dfe8739d
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-09-27 20:34:13 -0500

    Se agregaron comentarios y se hicieron cambios en el nombre de las variables

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -4,22 +4,23 @@ import { useEffect, useState, useRef} from "react";
 import { getCountries, createActivity, getActivities, deleteActivities } from '../../redux/actions';
 import validate from "./validate"; //importo la funcion de validaciones para mi form
 import style from "./Form.module.css"
-/* import soundFile from '../../audio/Crear.mp3' */
 
-
+//realiza la recarga de la página actual del navegador web. 
+//El parámetro false que se pasa a window.location.reload indica que la recarga se realice desde la caché del navegador, 
+//es decir, se utiliza la versión almacenada en la memoria del navegador en lugar de solicitar la página nuevamente al servidor.
 const reload = () => {
     window.location.reload(false);
 }
-//realiza la recarga de la página actual del navegador web. El parámetro false que se pasa a window.location.reload indica que la recarga se realice desde la caché del navegador, es decir, se utiliza la versión almacenada en la memoria del navegador en lugar de solicitar la página nuevamente al servidor.
 
 const Form = () => {
     const dispatch = useDispatch();
     const countriesName = useSelector((state) => state.countries);
-    const countriesorden = countriesName.sort((a, b) => a.name.localeCompare(b.name))
-    //selecciona el estado countries declarado en el initialState y lo ordeno alfabeticamente
+    //ordena alfabeticamente los paises
+    const countriesOrden = countriesName.sort((a, b) => a.name.localeCompare(b.name))
+
     const theActivities = useSelector((state) => state.activities);
-    const activitiesorden = theActivities.sort((a, b) => a.name.localeCompare(b.name))
-    //selecciona el estado activities declarado en el initialState y lo ordeno alfabeticamente
+    //ordena alfabeticamente las actividades
+    const activitiesOrden = theActivities.sort((a, b) => a.name.localeCompare(b.name))
 
     const [input, setInput] = useState({
         name: '', 
@@ -31,6 +32,10 @@ const Form = () => {
     
     const [errors, setErrors] = useState({});
     
+    //se ejecuta cuando se produce un cambio en el input o select, indica que se guarde dentro del estado input, 
+    //en la propiedad con el mismo nombre que tiene event.target.name, 
+    //y llama a la función setErrors para realizar validaciones en el nuevo estado input.
+
     const handleChange = (event) => {
         setInput({
             ...input,
@@ -41,8 +46,10 @@ const Form = () => {
             [event.target.name]: event.target.value
         }))
     }
-    //se ejecuta cuando se produce un cambio en el input o select, indica que se guarde dentro del estado input, en la propiedad con el mismo nombre que tiene event.target.name ( por ej: 'name'), el valor proporcionado en el input o select y llama a la función setErrors para realizar validaciones en el nuevo estado input.
     
+    //se ejecuta cuando se selecciona el pais o los paises donde se realiza la nueva actividad. 
+    //Concatena el valor proporcionado en el select con los valores previos en la propiedad countryId, 
+    //llama a la función setErrors para realizar validaciones en el nuevo estado input.   
     
     const handleSelectCountries = (event) => {
         setInput({
@@ -55,18 +62,21 @@ const Form = () => {
         }))
     }
 
-    //se ejecuta cuando se selecciona el pais o los paises donde se realiza la nueva actividad. Concatena el valor proporcionado en el select con los valores previos en la propiedad countryId, llama a la función setErrors para realizar validaciones en el nuevo estado input.   
     
 
-    const handleSubmit = (event) => {
-        event.preventDefault(); //se evita que la página se recargue al enviar el formulario.
+    const handleSubmit = (event) => { //se ejecuta cuando se envía el formulario de creación de actividad
+        event.preventDefault();
         const errorSave = validate(input); // se asigna el resultado de la funcion validate, pasandole input como parametro.
-        const existName = theActivities.find(activity => activity.name.toLowerCase() === input.name.toLowerCase()) ? 1 : 0; //si hay alguna actividad con el mismo nombre que el valor ingresado en el campo input.name, existName se establece en 1, de lo contrario, se establece en 0.
-        if (existName === 1) alert("Activity name already exists");// si hay una actividad con el mismo nombre 
-        else if (Object.values(errorSave).length !== 0) alert("You must fullfill all the required conditions");//Si el objeto errorSave tiene algún valor (Object.values() convierte el objeto en un array), se muestra una alerta al usuario indicando que deben cumplirse todas las condiciones requeridas. ¡¡¡YA NO ES NECESARIO PORQUE DESHABILITE EL BOTON!!!!
+
+        //si hay alguna actividad con el mismo nombre que el valor ingresado en el campo input.name, existName se establece en 1, de lo contrario, se establece en 0.
+        const existName = theActivities.find(activity => activity.name.toLowerCase() === input.name.toLowerCase()) ? 1 : 0; 
+
+        if (existName === 1) {
+            alert("Activity already exists");
+        } 
         else {
             dispatch(createActivity(input)) //se despacha la action pasandole como argumento "input", que es un objeto con todo lo necesario para crear una nueva actividad. 
-            alert('Activity Created!')//se muestra una alerta al usuario indicando que se ha creado la actividad con exito.
+            alert('Activity Created')
             setInput({
                 name: '', 
                 difficulty: '', 
@@ -74,44 +84,43 @@ const Form = () => {
                 season: '',
                 countryId: []
             }) // borrar los datos ingresados en el formulario.
-            reload(); //recargar la lista de actividades después de crear una nueva actividad.
+            reload(); //recargar 
         }
     }
 
     //------------------------Delete Activity------------------------
     const [delAct, setDelAct] = useState('');
 
+    //se ejecuta cuando se selecciona una actividad para eliminar en el campo de selección correspondiente. 
+    //Actualiza el estado delAct con el nombre de la actividad seleccionada.
     const handleSelectDelete = (event) => {
         setDelAct(event.target.value)
     }
-    //se ejecuta cuando se selecciona una actividad para eliminar en el campo de selección correspondiente. Actualiza el estado delAct con el nombre de la actividad seleccionada.
 
     const handleSubmitDelete = (event) => {
         event.preventDefault();
-        if (delAct.length <= 0) alert("You must select an activity to delete"); //¡¡¡YA NO ES NECESARIO PORQUE DESHABILITE EL BOTON!!!!
-        else {
-            dispatch(deleteActivities(delAct));
-            alert('Activity Deleted!')
-            setDelAct('');
-            reload();
-        }
+        dispatch(deleteActivities(delAct))
+        alert('Activity Deleted!')
+        setDelAct('');
+        reload();
     }
     
     //------------------------useEffect------------------------
+
+    //obtiene la lista de países del servidor cuando el componente se monta por primera vez
     useEffect(() => {
         dispatch(getCountries())
     }, [dispatch]);
-    // se utiliza para obtener la lista de países del servidor cuando el componente se monta por primera vez
     
+    //obtiene la lista de actividades del servidor cuando el componente se monta por primera vez
     useEffect(() => {
         dispatch(getActivities())
     }, [dispatch]);
-    // se utiliza para obtener la lista de actividades del servidor cuando el componente se monta por primera vez
 
+    // se utiliza para eliminar las actividades almacenadas
     useEffect(() => {
         dispatch(deleteActivities())
     }, [dispatch]);
-    // se utiliza para eliminar las actividades almacenadas
 
     return (
         <div className={style.contenedor}>
@@ -187,7 +196,7 @@ const Form = () => {
                         <select onChange={handleSelectCountries}>
                             <option value="" disabled selected>Select country</option>  
                             {/* Esta opción deshabilitada y seleccionada por defecto sirve como un texto de instrucción para que el usuario seleccione al menos un país. No se puede seleccionar y no tiene ningún valor asociado. */}
-                            {countriesorden.map((country) => (
+                            {countriesOrden.map((country) => (
                                 <option value={country.id}>{country.name}</option>
                                 // se generan opciones para el menú desplegable. Cada objeto country genera una opción con su id como valor y su name como texto visible para el usuario. 
                             ))}
@@ -224,7 +233,7 @@ const Form = () => {
                             <div>
                                 <select onChange={handleSelectDelete}>
                                     <option value="" disabled selected>Activity</option>
-                                    {activitiesorden && activitiesorden.map((activity) => {
+                                    {activitiesOrden && activitiesOrden.map((activity) => {
                                         return (
                                             <option value={activity.name}>{activity.name}</option>
                                         )
