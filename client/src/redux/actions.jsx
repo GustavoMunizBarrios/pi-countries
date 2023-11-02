3700f9661da0789985c52beec325b1c9096732ac
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-11-01 20:58:00 -0600

    Deploy frontend

--- client/src/redux/actions.jsx
+++ client/src/redux/actions.jsx
@@ -1,31 +1,35 @@
 import { GET_ACTIVITIES, DELETE_ACTIVITIES, GET_COUNTRIES, GET_COUNTRY_DETAIL, CREATE_ACTIVITY,SEARCH_COUNTRY,FILTER_COUNTRY, ORDER_BY_NAME,} from "./action-types";
 import axios from 'axios';
 
+const instance = axios.create({
+    // baseURL: 'http://localhost:3001/'
+    baseURL: 'https://pi-countries-production-b421.up.railway.app'
+  });
 
 export const getCountries = () => {
     return async (dispatch) => {
-        const {data} = await axios.get('http://localhost:3001/countries');
+        const {data} = await instance.get('countries');
         return dispatch({ type: GET_COUNTRIES, payload: data });
     }
 };
 
 export const getCountryDetail = (id) => {
     return async (dispatch) => {
-        let info = await axios.get(`http://localhost:3001/countries/${id}`);
+        let info = await instance.get(`countries/${id}`);
         return dispatch({ type: GET_COUNTRY_DETAIL, payload: info.data });
     }
 }
 
 export const createActivity = (payload) => {
     return async (dispatch) => {
-        let info = await axios.post('http://localhost:3001/activities',payload);
+        let info = await instance.post('activities',payload);
         return dispatch({ type: CREATE_ACTIVITY, payload: info.data });
     }
 }
 
 export const getActivities = (payload) => {
     return async (dispatch) => {
-        let info = await axios.get('http://localhost:3001/activities', payload);
+        let info = await instance.get('activities', payload);
         return dispatch({ type: GET_ACTIVITIES, payload: info.data });
     }
 }
@@ -33,7 +37,7 @@ export const getActivities = (payload) => {
 export const deleteActivities = (payload) => {
     return async (dispatch) => {
         try {
-            let info = await axios.delete(`http://localhost:3001/activities?name=${payload}`);
+            let info = await instance.delete(`activities?name=${payload}`);
             return dispatch({ type: DELETE_ACTIVITIES, payload: info.data })
         } catch (error) {
             console.log('Error deleting activity', error);
@@ -44,7 +48,7 @@ export const deleteActivities = (payload) => {
 export const searchCountry = (payload) => {
     return async (dispatch) => {
         try {
-            let info = await axios.get(`http://localhost:3001/countries/?name=${payload}`);
+            let info = await instance.get(`countries/?name=${payload}`);
             return dispatch({ type: SEARCH_COUNTRY, payload: info.data });
         } catch (error) {
             console.log('Error when searching for a country', error);
