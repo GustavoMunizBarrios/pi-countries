76cbf1623c6756bd969a9f1bd2e75531e2e95bcb
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-11-14 14:04:40 -0600

    Added proxy server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const axios = require("axios");
+/* const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
 const DB_upload = require('./DB_upload')
@@ -16,4 +16,36 @@ conn
       })
 }).catch(error => console.error(error))
 
+module.exports = server; */
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const server = require("./src/server");
+const { conn } = require('./src/db.js');
+const DB_upload = require('./DB_upload')
+const port = process.env.PORT || 3001;
+
+// Create a new Express application for your proxy server
+const app = express();
+
+// Set up middleware to forward requests
+app.use('/api', createProxyMiddleware({ target: 'http://localhost:5000', changeOrigin: true }));
+app.use('/', createProxyMiddleware({ target: 'http://localhost:' + port, changeOrigin: true }));
+
+// Start your application's server
+conn
+  .sync({ force: false })
+  .then(async() => {
+      await DB_upload();
+      server.listen(port, "0.0.0.0", () => {
+        console.log(`Server listening on port ${port}`);
+      })
+}).catch(error => console.error(error))
+
+// Start the proxy server
+const proxyPort = process.env.PROXY_PORT || 3000;
+app.listen(proxyPort, () => {
+  console.log(`Proxy server listening on port ${proxyPort}`);
+});
+
 module.exports = server;
+
